2efd8004c4d2a050213d3feec97b2e1da69726db
DhairyaLGandhi <dhairya@juliacomputing.com>
2024-05-30 02:53:11 +0530

    chore: store and convert types from AbstractArray

--- src/array.jl
+++ src/array.jl
@@ -3,15 +3,20 @@ hasportion(::Constants, ::AbstractArray) = false
 hasportion(::Caches, ::AbstractArray) = false
 hasportion(::Discrete, ::AbstractArray) = false
 
-struct ArrayRepack{T}
+struct ArrayRepack{T, Ty}
     sz::T
+    type::Ty
 end
 function (f::ArrayRepack)(A)
     @assert length(A) == prod(f.sz)
-    reshape(A, f.sz)
+    reshape(convert(f.type, A), f.sz)
 end
 
-canonicalize(::Tunable, p::AbstractArray) = vec(p), ArrayRepack(size(p)), true
+function canonicalize(::Tunable, p::AbstractArray)
+    arr = collect(p)
+    vec(arr), ArrayRepack(size(p), typeof(p)), true
+end
+canonicalize(::Tunable, p::Array) = vec(p), ArrayRepack(size(p), typeof(p)), true
 canonicalize(::Constants, p::AbstractArray) = nothing, nothing, nothing
 canonicalize(::Caches, p::AbstractArray) = nothing, nothing, nothing
 canonicalize(::Discrete, p::AbstractArray) = nothing, nothing, nothing
