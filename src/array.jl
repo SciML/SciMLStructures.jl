68e9b5ce6e3011ee996b8053283babb2ddfc60f0
DhairyaLGandhi <dhairya@juliacomputing.com>
2024-05-30 03:13:12 +0530

    chore: revert changes to ArrayRepack

--- src/array.jl
+++ src/array.jl
@@ -3,20 +3,15 @@ hasportion(::Constants, ::AbstractArray) = false
 hasportion(::Caches, ::AbstractArray) = false
 hasportion(::Discrete, ::AbstractArray) = false
 
-struct ArrayRepack{T, Ty}
+struct ArrayRepack{T}
     sz::T
-    type::Ty
 end
 function (f::ArrayRepack)(A)
     @assert length(A) == prod(f.sz)
-    reshape(convert(f.type, A), f.sz)
+    reshape(A, f.sz)
 end
 
-function canonicalize(::Tunable, p::AbstractArray)
-    arr = collect(p)
-    vec(arr), ArrayRepack(size(p), typeof(p)), true
-end
-canonicalize(::Tunable, p::Array) = vec(p), ArrayRepack(size(p), typeof(p)), true
+canonicalize(::Tunable, p::AbstractArray) = vec(p), ArrayRepack(size(p)), true
 canonicalize(::Constants, p::AbstractArray) = nothing, nothing, nothing
 canonicalize(::Caches, p::AbstractArray) = nothing, nothing, nothing
 canonicalize(::Discrete, p::AbstractArray) = nothing, nothing, nothing
