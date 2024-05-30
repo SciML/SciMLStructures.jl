8105f6ad823deeb54a970893d2454c853662605d
DhairyaLGandhi <dhairya@juliacomputing.com>
2024-05-30 16:16:02 +0530

    chore: add type to ArrayRepack

--- src/array.jl
+++ src/array.jl
@@ -3,15 +3,16 @@ hasportion(::Constants, ::AbstractArray) = false
 hasportion(::Caches, ::AbstractArray) = false
 hasportion(::Discrete, ::AbstractArray) = false
 
-struct ArrayRepack{T}
+struct ArrayRepack{T, Ty}
     sz::T
+    type::Ty
 end
 function (f::ArrayRepack)(A)
     @assert length(A) == prod(f.sz)
-    reshape(A, f.sz)
+    reshape(convert(f.type, A), f.sz)
 end
 
-canonicalize(::Tunable, p::AbstractArray) = vec(p), ArrayRepack(size(p)), true
+canonicalize(::Tunable, p::AbstractArray) = vec(p), ArrayRepack(size(p), typeof(A)), true
 canonicalize(::Constants, p::AbstractArray) = nothing, nothing, nothing
 canonicalize(::Caches, p::AbstractArray) = nothing, nothing, nothing
 canonicalize(::Discrete, p::AbstractArray) = nothing, nothing, nothing
